416f5e355bc11c4c9a52ad485bcc880234bcfe72
jonathan <jonathangea09@gmail.com>
2025-12-29 23:44:01 +0700

    refactor: remove unused RouterLink import from HomePageComponent

--- src/app/public/home/home.ts
+++ src/app/public/home/home.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
